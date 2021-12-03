e2e7bb57219797a1c728663d6af7b597dd5f4733
dt <dhrumilsanjaythakkar@gmail.com>
2021-12-03 10:25:08 +0530

    Build Commit

---
+++ build/precache-manifest.2f422771e9521430db680d8431362d22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d382ce9058613a2a2b82b8ee525bd734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78724884c97cc370b3aa",
+    "url": "/static/css/main.62008f7a.chunk.css"
+  },
+  {
+    "revision": "34ce753e5fdac7cad7f6",
+    "url": "/static/js/2.94013fcf.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.94013fcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78724884c97cc370b3aa",
+    "url": "/static/js/main.9dfb3131.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
